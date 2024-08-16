aa44383fb8f9594789f3347942b5b7f8e7add649
AscheKefyalew <aschalewkefyalew@gmail.com>
2024-08-17 00:16:16 +0300

    relation

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -8,14 +8,3 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     # Other URL patterns for your app
 ]
-
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('admin/', views.admin_view, name='admin_view'),
-    path('librarian/', views.librarian_view, name='librarian_view'),
-    path('member/', views.member_view, name='member_view'),
-]
-
