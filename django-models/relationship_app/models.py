1013e395e1ac81f0462637af7e16bcd97684179a
AscheKefyalew <aschalewkefyalew@gmail.com>
2024-08-17 22:35:59 +0300

    relation

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -27,6 +27,7 @@ class Librarian(models.Model):
     def __str__(self):
         return self.name
 
+    
 
 from django.db import models
 from django.contrib.auth.models import User
@@ -37,9 +38,8 @@ class UserProfile(models.Model):
         ('Librarian', 'Librarian'),
         ('Member', 'Member'),
     ]
-    
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
 
     def __str__(self):
-        return self.user.username
+        return f"{self.user.username} - {self.role}"
