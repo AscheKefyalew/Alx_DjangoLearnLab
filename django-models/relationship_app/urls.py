0f66e6582f8fd8c913a58ece96440e870318be9c
AscheKefyalew <aschalewkefyalew@gmail.com>
2024-08-18 22:05:39 +0300

    Custom Permissions

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -34,7 +34,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add-book/', views.add_book, name='add_book'),
-    path('edit-book/<int:pk>/', views.edit_book, name='edit_book'),
-    path('delete-book/<int:pk>/', views.delete_book, name='delete_book'),
+    path('add-book/', views.add_book, name='add_book'),  # URL for adding a book
+    path('edit-book/<int:pk>/', views.edit_book, name='edit_book'),  # URL for editing a book
+    path('delete-book/<int:pk>/', views.delete_book, name='delete_book'),  # URL for deleting a book
 ]
