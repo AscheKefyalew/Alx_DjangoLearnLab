8c55c7fb4a0e933c3eb2af0e0bf8bcde259010cc
AscheKefyalew <aschalewkefyalew@gmail.com>
2024-08-18 22:49:26 +0300

    custom permission

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -29,12 +29,22 @@ urlpatterns = [
     path('member/', views.member_view, name='member_view'),
 ]
 
-# Define the URL patterns for the secured views
+# # Define the URL patterns for the secured views
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('add-book/', views.add_book, name='add_book'),  # URL for adding a book
+#     path('edit-book/<int:pk>/', views.edit_book, name='edit_book'),  # URL for editing a book
+#     path('delete-book/<int:pk>/', views.delete_book, name='delete_book'),  # URL for deleting a book
+# ]
+
 from django.urls import path
-from . import views
+from .views import add_book, edit_book, delete_book, BookListView
 
 urlpatterns = [
-    path('add-book/', views.add_book, name='add_book'),  # URL for adding a book
-    path('edit-book/<int:pk>/', views.edit_book, name='edit_book'),  # URL for editing a book
-    path('delete-book/<int:pk>/', views.delete_book, name='delete_book'),  # URL for deleting a book
+    path('books/', BookListView.as_view(), name='book_list'),
+    path('books/add/', add_book, name='add_book'),
+    path('books/<int:pk>/edit/', edit_book, name='edit_book'),
+    path('books/<int:pk>/delete/', delete_book, name='delete_book'),
 ]
