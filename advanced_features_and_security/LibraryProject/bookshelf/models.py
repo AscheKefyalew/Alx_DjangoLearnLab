478c8016df2d3e9c2b0c7f3bc6266f98fe959ac3
AscheKefyalew <aschalewkefyalew@gmail.com>
2024-08-24 15:11:11 +0300

    custom user model

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
 
 # Create your models here.
 
@@ -17,3 +18,19 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, date_of_birth, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, date_of_birth=date_of_birth, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, date_of_birth, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, email, date_of_birth, password, **extra_fields)
+
