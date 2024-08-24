cc61bccd3904a3b8d12ed665f3bc303f42f97e76
AscheKefyalew <aschalewkefyalew@gmail.com>
2024-08-24 22:17:39 +0300

    managing permisions and groups

--- advanced_features_and_security/LibraryProject/LibraryProject/urls.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/urls.py
@@ -28,5 +28,8 @@ urlpatterns = [
     path('add_book/', views.add_book, name='add_book'),
     path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
     path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
+    path('book_list/', views.book_list, name='book_list'),
+    path('books/', views.books, name='books'),
 ]
+
 
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -19,3 +19,13 @@ def delete_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     # Logic to delete the book
     return render(request, 'relationship_app/delete_book.html', {'book': book})
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+def books(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/books.html', {'books': books})
+
+
