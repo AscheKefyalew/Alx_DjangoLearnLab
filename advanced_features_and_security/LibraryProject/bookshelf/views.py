15b1f1c15edc3dda25b42f74dcdf58e5a94d65ac
AscheKefyalew <aschalewkefyalew@gmail.com>
2024-08-24 22:49:26 +0300

    best practice

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, get_object_or_404
 from .models import Book
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+from .forms import ExampleForm
+
 
 @permission_required('relationship_app.can_create', raise_exception=True)
 def add_book(request):
