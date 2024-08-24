7fe334e52c33a3b2df4bb56740362207c5aca7f8
AscheKefyalew <aschalewkefyalew@gmail.com>
2024-08-24 22:04:19 +0300

    managing permisions and groups

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, get_object_or_404
+from .models import Book
 
-# Create your views here.
+@permission_required('relationship_app.can_create', raise_exception=True)
+def add_book(request):
+    # Logic to add a new book
+    return render(request, 'relationship_app/add_book.html')
+
+@permission_required('relationship_app.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    # Logic to edit the book
+    return render(request, 'relationship_app/edit_book.html', {'book': book})
+
+@permission_required('relationship_app.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    # Logic to delete the book
+    return render(request, 'relationship_app/delete_book.html', {'book': book})
