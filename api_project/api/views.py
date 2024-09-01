01e5f71f3d0ff974ef0740202f121cc18f71c711
AscheKefyalew <aschalewkefyalew@gmail.com>
2024-09-01 15:42:18 +0300

    Authentication and Permissions

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import BasePermission
+from .permissions import IsAdminUserOrReadOnly
+
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
@@ -11,3 +14,13 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAdminUserOrReadOnly]
+
+class IsAdminUserOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+        return request.user and request.user.is_staff
+
+
