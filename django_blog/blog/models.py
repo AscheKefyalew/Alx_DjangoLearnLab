f015d491198a913509087932a14d7b8ca84fc9f9
AscheKefyalew <aschalewkefyalew@gmail.com>
2024-09-15 15:11:54 +0300

    added post model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,16 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.urls import reverse
+from django.utils import timezone
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
     def __str__(self):
         return self.title
-
-    def get_absolute_url(self):
-        return reverse('post_detail', kwargs={'pk': self.pk})
