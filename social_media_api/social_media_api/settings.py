5335949d42316794c14e7312753266c4a6798895
AscheKefyalew <aschalewkefyalew@gmail.com>
2024-09-21 19:55:57 +0300

    Added Posts and Comments Functionality

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -40,13 +40,17 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework',
     'accounts',
+    'posts',  # Add the posts app
 ]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,  # Customize page size
 }
+
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
 
--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),  # Include URLs from the accounts app
+    path('', include('posts.urls')),  # Post and Comment endpoints
 ]
