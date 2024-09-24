a807aab9b482c69c5d3efbd7971c35c1fe419670
AscheKefyalew <aschalewkefyalew@gmail.com>
2024-09-24 20:55:27 +0300

    deployment

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -97,6 +97,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'db_user',
+        'PASSWORD': 'db_password',
+        'HOST': 'localhost',  # E.g., 'localhost' or Heroku/AWS URL
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
 
